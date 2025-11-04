5307dfb580a3c8b881858ff31d73666e3085de4e
ant1cus <ant1cus@yandex.ru>
2025-11-04 16:35:12 +0300

    fix count page in account text

--- create_files.py
+++ create_files.py
@@ -346,14 +346,24 @@ def create_file(documents, data, pt_num, incoming_data, account_docs) -> dict:
                 doc.save(str(Path(data.finish_path.parent, data.finish_path.stem + '.pdf')))
                 doc.close()
                 os.remove(pdf_path)
-        if re.findall(r'сопроводит', data.name.lower()) or re.findall(r'опись', data.name.lower()):
-            pages = pages_count(data.finish_path, True)
-            page = pages['pages']
-            if page == 0:
-                errors.append(f"Для файла {data.name} подсчёт кол-ва страниц завершился с ошибкой: {pages['text']}")
-                errors.append(pages['trace'])
-            index_doc = documents.loc[documents['name'] == data.name].index[0]
+        # if re.findall(r'сопроводит', data.name.lower()) or re.findall(r'опись', data.name.lower()):
+        pages = pages_count(data.finish_path, True)
+        page = pages['pages']
+        if page == 0:
+            errors.append(f"Для файла {data.name} подсчёт кол-ва страниц завершился с ошибкой: {pages['text']}")
+            errors.append(pages['trace'])
+        index_doc = documents.loc[documents['name'] == data.name].index[0]
+        if documents.loc[index_doc, 'pages'] != page:
             documents.loc[index_doc, 'pages'] = page
+            if isinstance(documents.loc[index_doc, 'account_list_text'], str)\
+                    and len(documents.loc[index_doc, 'account_list_text']) > 0:
+                a_text = documents.loc[index_doc, 'account_list_text'].split('!')
+                if '/' in a_text[3]:
+                    text_in_page = a_text[3].split('/')
+                    a_text[3] = '/'.join([str(page), text_in_page[1]])
+                else:
+                    a_text[3] = str(page)
+                documents.loc[index_doc, 'account_list_text'] = '!'.join(a_text)
         if errors:
             return {'status': 'warning', 'text': errors, 'documents': documents}
         return {'status': 'success', 'text': f'Документ {data.name} заполнен и сохранён', 'documents': documents}
